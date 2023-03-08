8d953cdef7317aaac4fbd5c2c811375e815bd4d5
Naoshi Tomisaka <naoshi.st.paul@gmail.com>
2023-03-09 00:55:02 +0900

    split into items

--- lib/main.dart
+++ lib/main.dart
@@ -44,9 +44,32 @@ class BookmarksListWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
-        Text(bookmarksList[0].title),
-        Text(bookmarksList[1].title),
+        BookmarksItemWidget(bookmarksItem: bookmarksList[0]),
+        BookmarksItemWidget(bookmarksItem: bookmarksList[1]),
       ],
+    );
+  }
+}
+
+class BookmarksItemWidget extends StatelessWidget {
+  const BookmarksItemWidget({Key? key, required this.bookmarksItem})
+      : super(key: key);
+
+  final Bookmark bookmarksItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(bookmarksItem.title,
+              style: Theme.of(context).textTheme.titleLarge),
+          Text(bookmarksItem.link,
+              style: Theme.of(context).textTheme.bodyMedium),
+        ],
+      ),
     );
   }
 }
