cb692fce8a91caae5ab165187a3b4d9213f18dc4
Naoshi Tomisaka <naoshi.st.paul@gmail.com>
2023-03-09 12:02:37 +0900

    clean code

--- lib/page/view_bookmark_page.dart
+++ lib/page/view_bookmark_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import '../widget/view_web_page_widget.dart';
 
 class ViewBookmarksPage extends StatefulWidget {
   const ViewBookmarksPage({super.key});
@@ -17,55 +17,6 @@ class _ViewBookmarksPageState extends State<ViewBookmarksPage> {
       ),
       body: const ViewWebPageWidget(),
       //body: ,
-    );
-  }
-}
-
-class ViewWebPageWidget extends StatefulWidget {
-  const ViewWebPageWidget({super.key});
-
-  @override
-  State<ViewWebPageWidget> createState() => _ViewWebPageWidgetState();
-}
-
-class _ViewWebPageWidgetState extends State<ViewWebPageWidget> {
-  var _isLoadingWebPage = true;
-
-  late WebViewController controller = WebViewController()
-    ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    ..setBackgroundColor(const Color(0x00000000))
-    ..setNavigationDelegate(
-      NavigationDelegate(
-        onProgress: (int progress) {
-          // Update loading bar.
-        },
-        onPageStarted: (String url) {},
-        onPageFinished: (String url) {
-          setState(() {
-            _isLoadingWebPage = false;
-          });
-        },
-        onWebResourceError: (WebResourceError error) {},
-        onNavigationRequest: (NavigationRequest request) {
-          if (request.url.startsWith('https://www.youtube.com/')) {
-            return NavigationDecision.prevent;
-          }
-          return NavigationDecision.navigate;
-        },
-      ),
-    )
-    ..loadRequest(Uri.parse('https://flutter.dev'));
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children: <Widget>[
-        WebViewWidget(
-          controller: controller,
-        ),
-        _isLoadingWebPage ? const CircularProgressIndicator() : Container(),
-      ],
     );
   }
 }
---
+++ lib/widget/view_web_page_widget.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class ViewWebPageWidget extends StatefulWidget {
+  const ViewWebPageWidget({super.key});
+
+  @override
+  State<ViewWebPageWidget> createState() => _ViewWebPageWidgetState();
+}
+
+class _ViewWebPageWidgetState extends State<ViewWebPageWidget> {
+  var _isLoadingWebPage = true;
+
+  late WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setBackgroundColor(const Color(0x00000000))
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          // Update loading bar.
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {
+          setState(() {
+            _isLoadingWebPage = false;
+          });
+        },
+        onWebResourceError: (WebResourceError error) {},
+        onNavigationRequest: (NavigationRequest request) {
+          if (request.url.startsWith('https://www.youtube.com/')) {
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
+      ),
+    )
+    ..loadRequest(Uri.parse('https://flutter.dev'));
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        WebViewWidget(
+          controller: controller,
+        ),
+        _isLoadingWebPage ? const CircularProgressIndicator() : Container(),
+      ],
+    );
+  }
+}
