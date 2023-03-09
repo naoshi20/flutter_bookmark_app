1c12a37f3ce0716c4f3873549451f89e077f358b
Naoshi Tomisaka <naoshi.st.paul@gmail.com>
2023-03-09 11:43:19 +0900

    add web view loading

--- lib/page/view_bookmark_page.dart
+++ lib/page/view_bookmark_page.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class ViewBookmarksPage extends StatelessWidget {
-  ViewBookmarksPage({super.key});
+class ViewBookmarksPage extends StatefulWidget {
+  const ViewBookmarksPage({super.key});
 
-  final WebViewController controller = WebViewController()
+  @override
+  State<ViewBookmarksPage> createState() => _ViewBookmarksPageState();
+}
+
+class _ViewBookmarksPageState extends State<ViewBookmarksPage> {
+  var _isLoadingWebPage = true;
+
+  late WebViewController controller = WebViewController()
     ..setJavaScriptMode(JavaScriptMode.unrestricted)
     ..setBackgroundColor(const Color(0x00000000))
     ..setNavigationDelegate(
@@ -13,7 +20,11 @@ class ViewBookmarksPage extends StatelessWidget {
           // Update loading bar.
         },
         onPageStarted: (String url) {},
-        onPageFinished: (String url) {},
+        onPageFinished: (String url) {
+          setState(() {
+            _isLoadingWebPage = false;
+          });
+        },
         onWebResourceError: (WebResourceError error) {},
         onNavigationRequest: (NavigationRequest request) {
           if (request.url.startsWith('https://www.youtube.com/')) {
@@ -31,7 +42,15 @@ class ViewBookmarksPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("ViewBookMarks"),
       ),
-      body: WebViewWidget(controller: controller),
+      body: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          WebViewWidget(
+            controller: controller,
+          ),
+          _isLoadingWebPage ? const CircularProgressIndicator() : Container(),
+        ],
+      ),
       //body: ,
     );
   }
